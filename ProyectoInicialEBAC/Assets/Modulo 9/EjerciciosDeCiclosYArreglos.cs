c12193a0615b7ed4de7fbc77af4e1353051d2f9b
Rogelio Gomez <ing.rogelio.gomez@gmail.com>
2022-11-30 00:06:29 -0600

    Ejercicios Modulo 9 Terminados
    Complete los 3 ejercicios.

--- ProyectoInicialEBAC/Assets/Modulo 9/EjerciciosDeCiclosYArreglos.cs
+++ ProyectoInicialEBAC/Assets/Modulo 9/EjerciciosDeCiclosYArreglos.cs
@@ -34,25 +34,24 @@ public class EjerciciosDeCiclosYArreglos : MonoBehaviour
         // }
         #endregion
 
-        #region Segunda forma de hacer el ejercicio (Jesús)
-        // // En este código se imprime el resultado en la primer posición a y b, luego la segunda posición de a y b y al final la suma de la posición 1 y 2.
-        // int[] a = new int[2];
-        // int[] b = new int[2];
-        // int[] c = new int[2];
+        #region Segunda forma de hacer el ejercicio (Jesús y alguna modificación por mi)
+        int[] a = new int[2];
+        int[] b = new int[2];
+        int[] c = new int[2];
 
-        // for (int i = 0; i < a.Length; i++)
-        // {
-        //     a[i] = Random.Range(1, 15);
-        //     Debug.Log($"Valores en A: {a[i]}");
-        //     b[i] = Random.Range(1, 15);
-        //     Debug.Log($"Valores en B: {b[i]}");
-        // }
+        Debug.Log("-- PRIMER EJERCICIO --");
+        for (int i = 0; i < a.Length; i++)
+        {
+            a[i] = Random.Range(1, 100);
+            // Debug.Log($"Valores en A: {a[i]}");
+            b[i] = Random.Range(1, 100);
+            // Debug.Log($"Valores en B: {b[i]}");
+        }
 
-        // for (int i = 0; i < c.Length; i++)
-        // {
-        //     c[i] = a[i] + b[i];
-        //     Debug.Log(c[i]);
-        // }
+        for (int i = 0; i < c.Length; i++)
+        {
+            Debug.Log($"La suma de A:{a[i]} y B:{b[i]} es : {c[i] = a[i] + b[i]}");
+        }
         #endregion
 
         #region Tercera forma de hacer el ejercicio (Jesús)
@@ -76,32 +75,38 @@ public class EjerciciosDeCiclosYArreglos : MonoBehaviour
         #endregion
 
         #region 2do Ejercicio
-        // string[] stringArray = { "Hola", "Mundo", "en", "la", "consola", "de", "Unity" };
-        // string newText = " ";
+        string[] stringArray = { "Hola", "Mundo", "en", "la", "consola", "de", "Unity" };
+        string newText = " ";
 
-        // foreach (string simpleText in stringArray)
-        // {
-        //     newText += " " + simpleText;
-        //     Debug.Log(newText);
-        // }
+        Debug.Log("-- SEGUNDO EJERCICIO --");
+
+        foreach (string simpleText in stringArray)
+        {
+            newText += " " + simpleText;
+            Debug.Log(newText);
+        }
         #endregion
 
         #region 3er Ejercicio
-        int[,] a = { { 1, 2, 3 }, { 4, 5, 6 } };
-        int[] b = { 7, 8, 9 };
-        int[,] c = new int[2, 1];
+        int[,] arrayA = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int[] arrayB = { 7, 8, 9 };
+        int[,] arrayC = new int[2, 1];
 
-        for (int i = 0; i < a.GetLength(1); i++)
+        Debug.Log("-- TERCER EJERCICIO --");
+
+        for (int i = 0; i < arrayA.GetLength(1); i++)
         {
-            Debug.Log($"(D0) Multiplicación de {a[0, i]} y {b[i]} es : {a[0, i] * b[i]}");
-            Debug.Log($"(D1) Multiplicación de {a[1, i]} y {b[i]} es : {a[1, i] * b[i]}");
+            Debug.Log($"(D0) Multiplicación de {arrayA[0, i]} y {arrayB[i]} es : {arrayA[0, i] * arrayB[i]}");
+            Debug.Log($"(D1) Multiplicación de {arrayA[1, i]} y {arrayB[i]} es : {arrayA[1, i] * arrayB[i]}");
 
-            for (int j = 0; j < c.Length; j++)
+            for (int j = 0; j < arrayC.Length; j++)
             {
-                Debug.Log($"El resultado de la suma es : {c[j, i] += a[1, j] * b[i]}");
+                Debug.Log($"El resultado de la suma de c[{j}] es : {arrayC[j, 0] += arrayA[j, i] * arrayB[i]}");
             }
         }
+        #endregion
 
+        // Funciona con este for pero estoy fallando al principio DRY.
         // for (int i = 0; i < a.GetLength(1); i++)
         // {
         //     Debug.Log($"Multiplicación de {a[1, i]} y {b[i]} es : {a[1, i] * b[i]}");
@@ -111,6 +116,20 @@ public class EjerciciosDeCiclosYArreglos : MonoBehaviour
         //         Debug.Log($"El resultado de la suma es : {d[j] += a[1, i] * b[i]}");
         //     }
         // }
-        #endregion
+
+        // Esta solución es la que me puso Jesús en la plataforma.
+        // int[,] a = { { 1, 2, 3 }, { 4, 5, 6 } };
+        // int[] b = { 7, 8, 9 };
+        // int[,] c = new int[2, 1];
+
+        // for (int i = 0; i < a.GetLength(1); i++)
+        // {
+        //     for (int j = 0; j < c.Length; j++)
+        //     {
+        //         Debug.Log($"es el ciclo i: {i} | j: {j}");
+        //         Debug.Log($"multiplicación de {a[j, i]} por {b[i]} es : {a[j, i] * b[i]}");
+        //         Debug.Log($"la suma de c[{j}] es : {c[j, 0] += a[j, i] * b[i]}");
+        //     }
+        // }
     }
 }
